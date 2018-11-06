c2831261d6573e04df2d912e0209b8e9cd6dc0c1
vlad-md <kuzinvladik@gmail.com>
2018-11-06 14:49:47 +0100

    Fixed the script to meet the criteria (added function)

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,23 +1,28 @@
-pwd #just to see where we are working
-repnum=$(ls | wc -l) #number of files in the directory
-echo $repnum #for troubleshooting the functionality of the program
-number=-1 #set a negative number just because to start the while statement
-while ! [ $number -eq $repnum ]
-do 
-	read -p "How many files are in this directory?" number
-	while ! [ "$number" -eq "$number" ] 2> /dev/null
-	do
-	        echo "Sorry integers only"
+
+function guessinggame {
+	
+	#pwd #just to see where we are working
+	repnum=$(ls | wc -l) #number of files in the directory
+	#echo $repnum #for troubleshooting the functionality of the program
+	number=-1 #set a negative number just because to start the while statement
+
+	while ! [ $number -eq $repnum ]
+	do 
 		read -p "How many files are in this directory?" number
+		while ! [ "$number" -eq "$number" ] 2> /dev/null
+		do
+		        echo "Sorry integers only"
+			read -p "How many files are in this directory?" number
+		done
+		if [ $number -eq $repnum ]
+			then 
+				echo "Congradulations you guessed the number correctly"
+		elif [ $number -gt $repnum ]
+			then
+				echo "There are less files in the derictory"
+		elif [ $number -lt $repnum ]
+			then
+				echo "There are more files in the derictory"
+		fi
 	done
-	if [ $number -eq $repnum ]
-		then 
-			echo "Congradulations you guessed the number correctly"
-	elif [ $number -gt $repnum ]
-		then
-			echo "There are less files in the derictory"
-	elif [ $number -lt $repnum ]
-		then
-			echo "There are more files in the derictory"
-	fi
-done
+}
